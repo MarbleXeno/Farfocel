0ad4b347c2a4410a2fa6dae507b5f9087d20e950
MarbleXeno <marblexeno@tuta.io>
2022-05-07 20:54:11 +0200

    Centered the animated frcl logo

--- Sandbox/src/AppStates/SandboxState.cpp
+++ Sandbox/src/AppStates/SandboxState.cpp
@@ -3,10 +3,9 @@
 app::SandboxState::SandboxState(fr::StateManager& stateManager, sf::RenderWindow& renderWindow)
 	: m_stateManager(&stateManager), m_renderWindow(&renderWindow), m_animationManager(*m_renderWindow)
 {
-	m_resourceManager.loadTexture("animated_logo", "res/test_animation.png");
+	m_resourceManager.loadTexture("animated_logo", "res/farfocel_logo_texture_atlas.png");
 
-	m_resourceManager.loadTextureAtlas("texture_atlas", *m_resourceManager.getTexture("animated_logo"), { 2, 3 });
-	m_resourceManager.getTextureAtlas("texture_atlas")->setCustomFrameCount(5);
+	m_resourceManager.loadTextureAtlas("texture_atlas", *m_resourceManager.getTexture("animated_logo"), { 1, 5 });
 
 	m_animationManager.add("animated_farfocel_logo", m_sprite, *m_resourceManager.getTextureAtlas("texture_atlas"));
 	m_animationManager.init("animated_farfocel_logo", 5.f, true, 1, 5);
@@ -14,6 +13,10 @@ app::SandboxState::SandboxState(fr::StateManager& stateManager, sf::RenderWindow
 	fr::InputManager::bindKeyboard(sf::Keyboard::D, false, [=]() {m_animationManager.changeAnimationState("animated_farfocel_logo", fr::AnimationState::Paused); });
 	fr::InputManager::bindKeyboard(sf::Keyboard::A, false, [=]() {m_animationManager.changeAnimationState("animated_farfocel_logo", fr::AnimationState::Playing); });
 
+
+	m_sprite.setOrigin(m_sprite.getGlobalBounds().width / 2.f, m_sprite.getGlobalBounds().height / 2.f);
+	m_sprite.setScale({ 0.5f, 0.5f });
+	m_sprite.setPosition({ 640.f / 2.f, 480.f / 2.f });
 }
 
 app::SandboxState::~SandboxState()
